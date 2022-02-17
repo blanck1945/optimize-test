c2972a067c76bf964a091d5f3c7a2f078acad6cb
Augusto Pastrana <apastrana@auntap.com>
2022-02-17 00:01:52 -0300

    Add Optimize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,14 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Script from "next/script";
 import { useEffect } from "react";
-import ReactGa from 'react-ga'
+import ReactGa from "react-ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ReactGa.initialize("G-0XTEX6VKZF");
+  }, []);
 
-    useEffect(() => {
-      ReactGa.initialize('G-0XTEX6VKZF');
-    },[])
-
-    return (
+  return (
     <div>
       <Head>
         <title>Create Next App</title>
@@ -30,16 +29,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         strategy="afterInteractive"
         src="https://www.googleoptimize.com/optimize.js?id=OPT-5KVFPFT"
       ></Script> */}
- <script async
+      <script
+        async
         src="https://www.googleoptimize.com/optimize.js?id=OPT-5KVFPFT"
       ></script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-93277234-2"></script>
-<script dangerouslySetInnerHTML={{ __html:  `window.dataLayer = window.dataLayer || [];
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-93277234-2"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-0XTEX6VKZF', { 'optimize_id': 'OPT-5KVFPFT'});
-`}}>
-</script>
+`,
+        }}
+      ></script>
       <Component {...pageProps} />
     </div>
   );
