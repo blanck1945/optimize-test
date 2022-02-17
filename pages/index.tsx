1ea8f38306fa95409c3884cdac10368471651eaa
Augusto Pastrana <apastrana@auntap.com>
2022-02-17 00:16:58 -0300

    Add useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -7,20 +8,36 @@ import Script from "next/script";
 declare global {
   interface Window {
     google_optimize?: any;
-    datalayer?: any;
+    dataLayer?: any;
   }
 }
 
 const Home: NextPage = () => {
-  if (typeof window !== "undefined") {
-    const variant = window?.google_optimize?.get("GEoLHCcyS2id9fLsbvLKxg");
-    // Client-side-only code
-    console.warn({ variant });
+  const [variantState, setVariantState] = useState(null)
+
+  useEffect(() => {
+    const init = async() => {
+      if (window.dataLayer) {
+        await window.dataLayer.push({ event: "optimize.activate" });
+    }
+      const intervalId = setInterval(() => {
+      if (window.google_optimize !== undefined) {
+        const variant = window?.google_optimize?.get("GEoLHCcyS2id9fLsbvLKxg");
+        setVariantState(variant);
+        clearInterval(intervalId);
+      }
+    }, 100);
   }
+  init()
+  },[])
+
   return (
     <div className={styles.container}>
       <div>
         <h3>Home page</h3>
+        <span>
+        Variant:{variantState}
+        </span>
       </div>
     </div>
   );
