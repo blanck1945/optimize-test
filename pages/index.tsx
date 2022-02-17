e3999138a2388c571c53beb69c998744ad385b8b
Augusto Pastrana <apastrana@auntap.com>
2022-02-17 00:28:01 -0300

    Add variant option

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,31 +13,36 @@ declare global {
 }
 
 const Home: NextPage = () => {
-  const [variantState, setVariantState] = useState(null)
+  const [variantState, setVariantState] = useState(null);
 
   useEffect(() => {
-    const init = async() => {
+    const init = async () => {
       if (window.dataLayer) {
         await window.dataLayer.push({ event: "optimize.activate" });
-    }
-      const intervalId = setInterval(() => {
-      if (window.google_optimize !== undefined) {
-        const variant = window?.google_optimize?.get("GEoLHCcyS2id9fLsbvLKxg");
-        setVariantState(variant);
-        clearInterval(intervalId);
       }
-    }, 100);
-  }
-  init()
-  },[])
+      const intervalId = setInterval(() => {
+        if (window.google_optimize !== undefined) {
+          const variant = window?.google_optimize?.get(
+            "GEoLHCcyS2id9fLsbvLKxg"
+          );
+          setVariantState(variant);
+          clearInterval(intervalId);
+        }
+      }, 100);
+    };
+    init();
+  }, []);
 
   return (
     <div className={styles.container}>
       <div>
         <h3>Home page</h3>
-        <span>
-        Variant:{variantState}
-        </span>
+        <span>Variant:{variantState}</span>
+        {variantState === 1 ? (
+          <a href="https://web.pency.app/">Pency</a>
+        ) : (
+          <a href="https://auntap.com/">Tap</a>
+        )}
       </div>
     </div>
   );
